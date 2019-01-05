b53f65daefaf83d53bba4e27601cabc3a68af3ff
Andres Leonardo Arevalo Parra <Aarevalop@intergrupo.com>
2019-01-05 10:22:45 -0500

    Initial Commit

---
+++ PlatziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Andres Leonardo Arevalo on 1/5/19.
+//  Copyright © 2019 Andres Leonardo Arevalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
