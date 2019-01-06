90abfbb8f2e54600436f1270cf2719bd351c4137
Andres Leonardo Arevalo Parra <Aarevalop@intergrupo.com>
2019-01-06 17:45:01 -0500

    Add carthage support

--- PlatziFinanzas/Application/Animation/LottieViewController.swift
+++ PlatziFinanzas/Application/Animation/LottieViewController.swift
@@ -7,24 +7,17 @@
 //
 
 import UIKit
+import Lottie
 
 class LottieViewController: UIViewController {
 
+    @IBOutlet weak var aniamtionView: LOTAnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        aniamtionView.setAnimation(named: "day_of_the_dead_skull")
+        aniamtionView.loopAnimation = true
+        aniamtionView.play()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
